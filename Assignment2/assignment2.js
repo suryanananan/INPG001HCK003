2113c25fa2207870f4f780b527cebfb4daa338a0
nana <nanasury2405@bsi.ac.id>
2023-01-12 15:18:30 +0700

    done

--- Assignment2/assignment2.js
+++ Assignment2/assignment2.js
@@ -1,6 +1,27 @@
-let i = 0;
+// let i = 0;
 
-for (let i = 0; i<=100; i+=2)
-{
-    console.log('ini dia', i);
+// for (let i = 0; i<=100; i+=2)
+// {
+//     console.log('ini dia', i);
+// }
+
+let isEdit = false;
+
+const button = document.getElementById('edit');
+const hasil = document.getElementsByTagName('div');
+
+button.addEventListener('click', () => {
+console.log('jalan', isEdit);
+if (isEdit === true) {
+    isEdit = false;
+    document
+        .getElementById('form')
+        .setAttribute('class','hidden');
 }
+    else{
+        isEdit = true;
+        document
+        .getElementById('form')
+        .setAttribute('class','displayed')
+    }
+})
